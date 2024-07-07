b758bcffe72548d1f8044f299f7963e67a532ccc
antoan <a.chavdarov@magazinidar.com>
2024-07-07 23:50:07 +0300

    new

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ import cookieParser from "cookie-parser";
 import path from "path";
 const { MONGO_URL, PORT } = process.env;
 const app = express();
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //MIDDLEWARES
 //Middleware for handling CORS
@@ -58,14 +58,14 @@ mongoose
   });
 
 //GLOBAL ERROR HANDLER
-app.use((err, res, req, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+// app.use((err, res, req, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || "error";
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
+// });
 //SERVER
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
